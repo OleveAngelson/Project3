846e43eb06893e36c05c3c03c0b2fa77a651924c
Mas Bro <lauraimuet150@gmail.com>
2023-08-28 20:02:51 +0700

    Initial commit

---
+++ app/src/main/java/kelompok3/menyusahkansaja/MainActivity.java
@@ -0,0 +1,46 @@
+package kelompok3.menyusahkansaja;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import kelompok3.menyusahkansaja.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    Button btnLogOut;
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
+        btnLogOut = findViewById(R.id.btnLogout);
+        mAuth = FirebaseAuth.getInstance();
+
+        btnLogOut.setOnClickListener(view ->{
+            mAuth.signOut();
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user == null) {
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        }
+    }
+}
---
+++ app/src/test/java/kelompok3/menyusahkansaja/AppCompatActivity.java
@@ -0,0 +1,4 @@
+package kelompok3.menyusahkansaja;
+
+public class AppCompatActivity {
+}
